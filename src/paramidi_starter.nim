a9465cd4b8ca51b75ea1db58224fa60663b7a4c3
oakes <zach@sekao.net>
2020-08-14 09:29:59 -0400

    Bump paramidi and remove quotes

--- src/paramidi_starter.nim
+++ src/paramidi_starter.nim
@@ -7,12 +7,12 @@ const
   score =
     (piano,
       (tempo: 74),
-      (1/8, {`d-`, `a-`, e, fx}, a,
-       1/2, {fx, `d+`},
-       1/8, {`e-`, e, `c+`}, a,
+      (1/8, {-d, -a, e, fx}, a,
+       1/2, {fx, +d},
+       1/8, {-e, e, +c}, a,
        1/2, {c, e},
-       1/8, {`d-`, `a-`, e, fx}, a, `d+`, `cx+`, `e+`, `d+`, b, `cx+`,
-       1/2, {`e-`, c, a}, 1/2, {c, e}))
+       1/8, {-d, -a, e, fx}, a, +d, +cx, +e, +d, b, +cx,
+       1/2, {-e, c, a}, 1/2, {c, e}))
 
 when isMainModule:
   # get the sound font
