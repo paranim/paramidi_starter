0b15d0505eb2f1b626b5cb3826511738bbf5c646
oakes <zach@sekao.net>
2020-08-22 11:00:36 -0400

    Add a constant to control whether to write wav file to disk

--- src/paramidi_starter.nim
+++ src/paramidi_starter.nim
@@ -23,12 +23,17 @@ when isMainModule:
   # during dev, read it from the disk
   else:
     var sf = tsf_load_filename(paramidi_soundfonts.getSoundFontPath("generaluser.sf2"))
+  # render the score
   const sampleRate = 44100
   tsf_set_output(sf, TSF_MONO, sampleRate, 0)
   var res = render[cshort](compile(score), sf, sampleRate)
-  const padding = 500f # add a half second so it doesn't cut off abruptly
-  common.writeFile("output.wav", res.data, res.data.len.uint32, sampleRate)
-  common.play("output.wav", int(res.seconds * 1000f + padding))
-  # if you want to avoid writing to the disk, do this instead:
-  #let wav = common.writeMemory(res.data, res.data.len.uint32, sampleRate)
-  #common.play(wav, int(res.seconds * 1000f + padding))
+  # create the wav file and play it
+  const
+    writeToDisk = true # if false, the wav file will only exist in memory
+    padding = 500f # add a half second so it doesn't cut off abruptly
+  when writeToDisk:
+    common.writeFile("output.wav", res.data, res.data.len.uint32, sampleRate)
+    common.play("output.wav", int(res.seconds * 1000f + padding))
+  else:
+    let wav = common.writeMemory(res.data, res.data.len.uint32, sampleRate)
+    common.play(wav, int(res.seconds * 1000f + padding))
